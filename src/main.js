1652489e668c8b5067cf364b95f32976b603f04c
Ian fabs <ianfabs@outlook.com>
2019-02-07 03:46:40 +0100

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueDriver from './plugins/driver'
+import './plugins/theme.scss'
+
+Vue.use(VueDriver)
 
 Vue.config.productionTip = false
 
